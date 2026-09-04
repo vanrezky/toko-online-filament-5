d0e2697fe1b050fbfb9f7e5d63793777021a0af5
vanrezky <vanrezkysadewa77@gmail.com>
2026-09-05 01:18:52 +0700

    fix: remove duplicate key r2

--- config/filesystems.php
+++ config/filesystems.php
@@ -69,18 +69,6 @@ return [
             'throw' => false,
         ],
 
-        'r2' => [
-            'driver' => 's3',
-            'key' => env('R2_ACCESS_KEY_ID'),
-            'secret' => env('R2_SECRET_ACCESS_KEY'),
-            'region' => env('R2_DEFAULT_REGION', 'auto'),
-            'bucket' => env('R2_BUCKET'),
-            'endpoint' => env('R2_ENDPOINT'),
-            'url' => env('R2_URL'),
-            'use_path_style_endpoint' => true,
-            'throw' => true,
-        ],
-
     ],
 
     /*
